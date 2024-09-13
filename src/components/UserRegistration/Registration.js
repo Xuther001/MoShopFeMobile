e84702d3be0efa711f21f1dee1ae4b981df2f0eb
osmac <macos@osmacs-MBP.lan>
2024-09-13 18:06:37 +0700

    added Registration.css for Registration.js

--- src/components/UserRegistration/Registration.js
+++ src/components/UserRegistration/Registration.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './Registration.css';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -37,89 +38,92 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="firstname"
-        placeholder="First Name"
-        value={formData.firstname}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="lastname"
-        placeholder="Last Name"
-        value={formData.lastname}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={formData.email}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        value={formData.username}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="streetAddress"
-        placeholder="Street Address"
-        value={formData.streetAddress}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="city"
-        placeholder="City"
-        value={formData.city}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="state"
-        placeholder="State"
-        value={formData.state}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="postalCode"
-        placeholder="Postal Code"
-        value={formData.postalCode}
-        onChange={handleChange}
-        required
-      />
-      <input
-        type="text"
-        name="country"
-        placeholder="Country"
-        value={formData.country}
-        onChange={handleChange}
-        required
-      />
-      <button type="submit">Register</button>
-    </form>
+    <div className="registration-container">
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstname"
+          placeholder="First Name"
+          value={formData.firstname}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="lastname"
+          placeholder="Last Name"
+          value={formData.lastname}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="streetAddress"
+          placeholder="Street Address"
+          value={formData.streetAddress}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="city"
+          placeholder="City"
+          value={formData.city}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="state"
+          placeholder="State"
+          value={formData.state}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="postalCode"
+          placeholder="Postal Code"
+          value={formData.postalCode}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="country"
+          placeholder="Country"
+          value={formData.country}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
   );
 };
 
