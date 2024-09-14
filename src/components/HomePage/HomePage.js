96b444bbf1fb8c3d9bdd7063dc76e6b0b112db61
osmac <macos@osmacs-MBP.lan>
2024-09-15 01:34:49 +0700

    minor cosmetic changes

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate  } from 'react-router-dom';
 import ProductList from '../ProductList/ProductList';
 import './HomePage.css';
 
 function HomePage() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    setIsLoggedIn(false);
+    navigate('/');
+  };
+
   return (
     <div className="home-page">
-      <Link to="/register" className="signup-link">Sign Up</Link>
-      <Link to="/login" className="login-link">Login</Link>
+      <div className="auth-buttons">
+        {!isLoggedIn ? (
+          <>
+            <Link to="/login" className="login-link">Login</Link>
+            <Link to="/register" className="signup-link">Sign Up</Link>
+          </>
+        ) : (
+          <button className="logout-button" onClick={handleLogout}>Log Out</button>
+        )}
+      </div>
       <ProductList />
     </div>
   );
