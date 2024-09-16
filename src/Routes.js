a9ba0b12f0f631d606dcc7d9888622034bbcb164
osmac <macos@osmacs-MBP.lan>
2024-09-16 07:16:07 +0700

    added MyCart component

---
+++ src/Routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Registration from './Registration';
+import Register from './components/UserRegistration/Register';
+import ProductList from './components/ProductList/ProductList';
+import ProductDetails from './components/ProductDetails/ProductDetails'; // Make sure to import ProductDetails if needed
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/products" element={<ProductList />} />
+        {/* Add other routes as needed */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- src/components/MyCart/MyCart.js
+++ src/components/MyCart/MyCart.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../../configs/axiosConfig';
+import './MyCart.css';
 
 function MyCart() {
   const [cartItems, setCartItems] = useState([]);
@@ -27,18 +28,29 @@ function MyCart() {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div>
+    <div className="my-cart-container">
       <h2>My Cart</h2>
       {cartItems.length > 0 ? (
         cartItems.map((item) => (
-          <div key={item.id}>
-            <p>Product ID: {item.productId}</p>
-            <p>Quantity: {item.quantity}</p>
-            <p>Total Price: ${item.totalPrice}</p>
+          <div key={item.productName} className="cart-item">
+            <img src={item.productImageUrl} alt={item.productName} />
+            <div className="cart-item-details">
+              <p className="item-value item-name">{item.productName}</p>
+              <p className="item-label">Description:</p>
+              <p className="item-value item-description">{item.productDescription}</p>
+              <div className="quantity-row">
+                <p className="item-label">Quantity:</p>
+                <p className="item-value item-quantity">{item.quantity}</p>
+              </div>
+              <div className="price-row">
+                <p className="item-label">Total Price:</p>
+                <p className="item-value item-price">${item.totalPrice.toFixed(2)}</p>
+              </div>
+            </div>
           </div>
         ))
       ) : (
-        <p>Your cart is empty.</p>
+        <p className="empty-cart-message">Your cart is empty.</p>
       )}
     </div>
   );
