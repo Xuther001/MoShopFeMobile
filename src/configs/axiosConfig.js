a622434e3225b2fd1c752b33e7bc809b156f9a6b
Xuther001 <xuther@yahoo.com>
2024-09-25 06:28:26 +0700

    test

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -11,7 +11,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://ec2-3-235-3-82.compute-1.amazonaws.com:8080',
+  baseURL: 'http://ec2-3-235-3-82.compute-1.amazonaws.com:8080',
 });
 
 export default instance;
