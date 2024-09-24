6ffef6c6e8fe39fca34a606c66453325e1988577
Xuther001 <xuther@yahoo.com>
2024-09-24 13:06:54 +0700

    changed axiosConfig.js to be used on AWS Amplify

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -1,7 +1,17 @@
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: 'http://localhost:8080',
+// });
+
+// export default instance;
+
+
+// For AWS Amplify
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://ec2-44-200-11-149.compute-1.amazonaws.com:8080',
 });
 
 export default instance;
